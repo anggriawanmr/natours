44168e8de403bb362d263554d26c34fa7ade38b6
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-06 09:31:22 +0700

    Initial commit

---
+++ models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A name must be filled']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    lowercase: true,
+    unique: true,
+    validate: [validator.isEmail, 'Please fill a valid email address']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 8,
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // THIS ONLY WORK ON SAVE!!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
