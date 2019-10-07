44e608dc8a1f4b56b232c91a20570ac9dd7aa36a
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-07 09:22:06 +0700

    add reset password function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
@@ -100,3 +101,19 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('No user found with email address', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
