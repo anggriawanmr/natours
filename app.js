6465dff8144aa144996e2f264a12d4c00cedb5b8
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-08 23:25:13 +0700

    Preventing Parameter Pollutin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,6 +38,20 @@ app.use(mongoSanitize());
 
 // Data sanitization agains XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    Whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // Serving Static files
 app.use(express.static(`${__dirname}/public`));
