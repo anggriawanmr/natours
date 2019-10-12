8731f526f42ba6a2ba691b839766ed8992d302ea
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-12 08:35:54 +0700

    Reversing

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,5 @@
 const Review = require('./../models/reviewModel');
 const factory = require('./handlerFactory');
-const catchAsync = require('./../utils/catchAsync');
 
 exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
@@ -9,22 +8,7 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-});
-
-// exports.getAllReviews = factory.getAll(Review);
+exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
