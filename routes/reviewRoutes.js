789e7481da0d98ba182f37bd141f8404dd81029d
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-12 09:41:20 +0700

    editing some access

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,12 +4,13 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-router.use(authController.protect);
+// router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
+    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -17,14 +18,7 @@ router
 
 router
   .route('/:id')
-  .get(reviewController.getReview)
-  .patch(
-    authController.restrictTo('user', 'admin'),
-    reviewController.updateReview
-  )
-  .delete(
-    authController.restrictTo('user', 'admin'),
-    reviewController.deleteReview
-  );
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview);
 
 module.exports = router;
