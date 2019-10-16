9d0a04d0d35664c2da917302c82ee5aeda7c70b0
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-16 23:04:28 +0700

    Adding login route

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -16,7 +16,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   //1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug })
     .populate({ path: 'reviews', fields: 'review rating user' })
@@ -29,3 +29,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
