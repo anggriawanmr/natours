3326c7029848585b95ce4db0c8774817335e1352
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-17 23:28:42 +0700

    Adding logout feature

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,27 @@
 
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form--login');
+const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 // VALUES
 
 // DELEGATION
 if (mapBox) {
-  const locations = JSON.parse(mapBox.dataset.locations);
+  // const locations = JSON.parse(mapBox.dataset.locations);
   displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
-}
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
