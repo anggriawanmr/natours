d70b6d1242a03f9bf12b685f7a43dabd327980fd
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-20 22:12:10 +0700

    Adding password reset feature

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -3,19 +3,21 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 // type is either 'password' or 'data'
-export const updateData = async (name, email) => {
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:8000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:8000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data Updated Successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
