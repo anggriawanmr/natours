e05ba96f8163e286446d751106552c4b6c0063d0
Anggriawan Marchvici Ridatama <anggriawanmr@gmail.com>
2019-10-21 21:42:45 +0700

    Adding Resizing Image Feature

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8937,7 +8937,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56176" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
